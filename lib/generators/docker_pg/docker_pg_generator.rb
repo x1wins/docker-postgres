56e45c605323b128b7d9ced707702e74e570ade1
Chang-Woo Rhee <changwoo.rhee@kooli.tv>
2019-12-19 15:05:07 -0800

    lib/generators/docker_pg/docker_pg_generator.rb

--- lib/generators/docker_pg/docker_pg_generator.rb
+++ lib/generators/docker_pg/docker_pg_generator.rb
@@ -1,3 +1,7 @@
-class DockerPgGenerator < Rails::Generators::NamedBase
+class DockerPgGenerator < Rails::Generators::Base
   source_root File.expand_path('templates', __dir__)
+
+  def copy_docker_pg_file
+    copy_file "docker_pg.rake", "lib/tasks/docker_pg.rake"
+  end
 end
